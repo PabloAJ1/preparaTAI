3b831177709d3acfa5ad649e549d6297dff9ff3e
Jackercito <jackercito@gmail.com>
2026-04-17 19:42:49 +0200

    commit

--- packages/backend/src/domains/preguntasDomain/application/signatures/GenerarListaPreguntasService.interface.ts
+++ packages/backend/src/domains/preguntasDomain/application/signatures/GenerarListaPreguntasService.interface.ts
@@ -1,6 +1,11 @@
 import { Pregunta } from "../../domain/entities/Pregunta";
+import { TTipoPreguntas } from "../../domain/enums/tipoPreguntas.enum";
 import { IPreguntaPobladaDto } from "../dtos/preguntaPoblada.dto";
 
 export interface IGenerarListaPreguntasService {
-	generar(preguntas: Pregunta[], idsDePagina: string[]): Promise<IPreguntaPobladaDto[]>
+	generar(
+		preguntas: Pregunta[], 
+		idsDePagina: string[],
+		tipoPregunta?: TTipoPreguntas
+	): Promise<IPreguntaPobladaDto[]>
 }
